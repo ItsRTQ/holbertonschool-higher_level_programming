c8299cf46226fcc082fcdc8e7188e2897c28851e
William <titanbabrisa@gmail.com>
2024-01-24 02:13:03 -0400

    task 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_last_digit(number):
+    if isinstance(number, int):
+        number = abs(number) % 10
+        print("{}".format(number), end="")
+        return number
+    else:
+        return None
