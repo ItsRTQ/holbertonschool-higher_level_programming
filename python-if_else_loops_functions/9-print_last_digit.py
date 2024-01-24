2e68cac90512d7a5a71c48699cce769e74ba5f7b
William <titanbabrisa@gmail.com>
2024-01-24 02:23:54 -0400

    task 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if isinstance(number, int):
-        number = abs(number) % 10
-        print("{}".format(number), end="")
-        return number
-    elif isinstance(number, str) and number.isdigit():
-        lastdigit = int(number) % 10
-        print("{}".format(lastdigit), end="")
-        return lastdigit
-    else:
-        return None
+    number = abs(number) % 10
+    print("{}".format(number), end="")
+    return number
