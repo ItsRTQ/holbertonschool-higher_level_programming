9a2d514eaba286b5d188b9bdcc392ce5552a9e96
William <titanbabrisa@gmail.com>
2024-01-23 22:59:33 -0400

    fixing

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 5,6, 12, 0
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[str.find('object-oriented programming'):-63]} with Python")
+str = f"{str.split()[5]} {str.split()[6]} {str.split()[12]} {str.split()[0]}"
 print(str)
