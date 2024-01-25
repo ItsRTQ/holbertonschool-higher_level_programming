742d1a47ca56e3204f6a8487d9b9cf03623a5f01
William <titanbabrisa@gmail.com>
2024-01-24 22:02:18 -0400

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,9 +2,10 @@
 from sys import argv
 argc = len(argv)
 result = 0
-if argc == 2:
-    print('0')
-else:
-    for i in range(1, argc):
-        result += int(argv[i])
-    print(f"{result}")
+if __name__ == "__main__":
+    if argc == 2:
+        print('0')
+    else:
+        for i in range(1, argc):
+            result += int(argv[i])
+        print(f"{result}")
