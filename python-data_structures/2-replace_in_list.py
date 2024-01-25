2a98cd5fb69e1e6187e3d5ab0728495e163f80a4
William <titanbabrisa@gmail.com>
2024-01-24 23:56:14 -0400

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    argc = 0
+    if my_list:
+        for number in my_list:
+            if argc == idx:
+                my_list[argc] = element
+                return my_list
+            argc += 1
+    return my_list
