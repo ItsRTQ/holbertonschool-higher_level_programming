bd2da88761f4892f2f908ecce72b37fe5ba74ce1
William <titanbabrisa@gmail.com>
2024-01-25 01:39:48 -0400

    task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    lenght = len(sentence)
+    if lenght > 0:
+        firstChar = sentence[0]
+    else:
+        firstChar = None
+    data = (lenght, firstChar)
+    return data
