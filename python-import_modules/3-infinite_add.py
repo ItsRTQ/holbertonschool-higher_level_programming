bd09b23df6db8a94ec73d4b5e9562394e6b8b44f
William <titanbabrisa@gmail.com>
2024-01-24 22:01:39 -0400

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from sys import argv
+argc = len(argv)
+result = 0
+if argc == 2:
+    print('0')
+else:
+    for i in range(1, argc):
+        result += int(argv[i])
+    print(f"{result}")
