a457ce4435e499e7ceb7be6541b0d83e3234042e
William <titanbabrisa@gmail.com>
2024-01-24 22:11:14 -0400

    task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import hidden_4
+all_names = dir(hidden_4)
+toPrint = []
+if __name__ == "__main__":
+    for i in all_names:
+        if i[0] != '_':
+            toPrint.append(i)
+    toPrint.sort()
+    for j in toPrint:
+        print(j)
