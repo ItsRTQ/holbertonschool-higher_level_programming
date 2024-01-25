b250382299e0b21e9ff24ca0a0dc21ad925531ba
William <titanbabrisa@gmail.com>
2024-01-25 01:55:46 -0400

    task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    argc = 0
+    if my_list:
+        for number in my_list:
+            if argc == idx:
+                del(my_list[argc])
+                return my_list
+            argc += 1
+    return my_list
