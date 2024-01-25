15a60f73e4b70cfd0142cbfa4a2e8452779ff3ad
William <titanbabrisa@gmail.com>
2024-01-24 21:00:19 -0400

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 a = 1
 b = 2
-exec(open('add_0.py').read())
+from add_0 import add
 result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, result))
