f1a3e438a0a4a3ecaa3335790a3bc2dfcf2c9203
William <titanbabrisa@gmail.com>
2024-01-24 23:32:17 -0400

    task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """
+    Print integers from the given list.
+
+    Args:
+        my_list (list): List of integers to be printed.
+
+    """
     if my_list:
         for number in my_list:
             print("{}".format(number))
-    else:
-        print()
