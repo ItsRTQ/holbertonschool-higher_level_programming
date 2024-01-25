c30d292a679ab7cbec2d0ef30802849640c9c096
William <titanbabrisa@gmail.com>
2024-01-24 21:37:41 -0400

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+argc = len(sys.argv)
+args = sys.argv
+print("{} arguments".format(argc - 1), end="")
+if argc - 1 != 0:
+    print(':')
+else:
+    print('.')
+for i in range(1, argc):
+    print("{}: {}".format(i, args[i]))
