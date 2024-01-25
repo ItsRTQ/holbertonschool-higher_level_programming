534eb74695ad256b6bfceef9af6822c3beeb2381
William <titanbabrisa@gmail.com>
2024-01-24 23:19:58 -0400

    task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list):
-    for i in my_list:
-        print("{}".format(int(i)))
+def print_list_integer(my_list=[]):
+    argc = 0
+    for numbers in my_list:
+        print("{}".format(my_list[argc]))
+        argc += 1
