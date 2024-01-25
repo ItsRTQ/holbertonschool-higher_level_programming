2bbca2a888134a21d1ec4eecb41ffc0c457eee3b
William <titanbabrisa@gmail.com>
2024-01-25 01:53:01 -0400

    task 9

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    check_list = []
+    for number in my_list:
+        if number % 2 == 0:
+            check_list.append(True)
+        else:
+            check_list.append(False)
+    return check_list
