a46a022966ddb0dad4991ae3a9abfbc05161e1b4
William <titanbabrisa@gmail.com>
2024-01-25 00:41:02 -0400

    task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    argc = 0
+    if new_list:
+        for number in new_list:
+            if argc == idx:
+                new_list[argc] = element
+                return new_list
+            argc += 1
+    return new_list
