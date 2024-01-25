ee959ea486e3f342c288fdd622c580a62f9c2a01
William <titanbabrisa@gmail.com>
2024-01-24 21:54:54 -0400

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ argc = len(argv)
 if __name__ == "__main__":
     print("{} argument".format(argc - 1), end="")
     if argc - 1 != 0:
-        if argc - 1 == 1:
+        if argc == 2:
             print(':')
         else:
             print('s:')
     else:
-        print('.')
+        print('s.')
     for i in range(1, argc):
         print("{}: {}".format(i, argv[i]))
