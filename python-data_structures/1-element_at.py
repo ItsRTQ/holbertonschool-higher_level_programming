d9b2bd77d30da05b43fc0ffb3412e2aa02fe0dae
William <titanbabrisa@gmail.com>
2024-01-24 23:49:53 -0400

    task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def element_at(my_list, idx):
+    argc = 0
+    if my_list:
+        for number in my_list:
+            if argc == idx:
+                return number
+            argc += 1
+    return None
