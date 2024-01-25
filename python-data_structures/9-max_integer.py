0b90e82523f66e94ae90bc7b459ca065d0a3a3a1
William <titanbabrisa@gmail.com>
2024-01-25 01:44:33 -0400

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    check = 0
+    if not my_list[0]:
+        return None
+    else:
+        for i in my_list:
+            if i > check:
+                check = i
+        return check
