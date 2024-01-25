ec6608c35c919a4d6ed4ec1ed538c1c57105fbd3
William <titanbabrisa@gmail.com>
2024-01-24 21:44:39 -0400

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import sys
-argc = len(sys.argv)
-args = sys.argv
+from sys import argv
+argc = len(argv)
 if __name__ == "__main__":
-    print("{} arguments".format(argc - 1), end="")
+    print("{} arguments.".format(argc - 1), end="")
     if argc - 1 != 0:
         print(':')
     else:
         print('.')
     for i in range(1, argc):
-        print("{}: {}".format(i, args[i]))
+        print("{}: {}".format(i, argv[i]))
