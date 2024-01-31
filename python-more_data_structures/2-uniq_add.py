f2428ade35d116ce7101df56c2c04688bd13b024
William <titanbabrisa@gmail.com>
2024-01-30 20:43:32 -0400

    task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if my_list:
+        skip = []
+        result = 0
+        for value in my_list:
+            if value in skip:
+                pass
+            else:
+                result += value
+                skip.append(value)
+        return result
+    else:
+        return None
