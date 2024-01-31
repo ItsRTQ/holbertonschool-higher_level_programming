36da3bf3a43be4d7effccce2d7acbfd77f7c9491
William <titanbabrisa@gmail.com>
2024-01-30 20:36:56 -0400

    task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if isinstance(my_list, list):
+        new_list = my_list.copy()
+        for index, value in enumerate(new_list):
+            if value == search:
+                new_list[index] = replace
+        return new_list
+    else:
+        return None
