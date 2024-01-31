c87e9e5d09159b7819ac4076d1fb830c44c9998b
William <titanbabrisa@gmail.com>
2024-01-30 20:54:34 -0400

    task 4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    diff_elements = []
+    for value in set_1:
+        if value in set_2:
+            pass
+        else:
+            diff_elements.append(value)
+    for value in set_2:
+        if value in set_1:
+            pass
+        else:
+            diff_elements.append(value)
+    return diff_elements
