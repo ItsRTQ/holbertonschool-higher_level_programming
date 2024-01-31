2bad574b8336053c269ede32067eab561a21d08d
William <titanbabrisa@gmail.com>
2024-01-30 23:10:20 -0400

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict):
+    if isinstance(a_dictionary, dict) and a_dictionary:
+        score_amount = 0
         for index, key in enumerate(a_dictionary):
             if index == 0:
                 score = key
             if a_dictionary[key] > a_dictionary[score]:
                 score = key
         if score:
-            return score
-        else:
-            return None
+            for key in a_dictionary:
+                if a_dictionary[key] == a_dictionary[score]:
+                    score_amount += 1
+            if score_amount == 1:
+                return score
+        return None
     else:
         return None
