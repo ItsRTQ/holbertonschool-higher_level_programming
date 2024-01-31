882a2d594f55105cd575271f6f695cb2ddd33382
William <titanbabrisa@gmail.com>
2024-01-30 20:49:48 -0400

    task 3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def common_elements(set_1, set_2):
+    common_elements = []
+    for value in set_1:
+        if value in set_2:
+            common_elements.append(value)
+    return common_elements
