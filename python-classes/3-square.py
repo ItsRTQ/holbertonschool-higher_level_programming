b91bb87bdd503ef271c54094e247b951eee22356
William <titanbabrisa@gmail.com>
2024-02-01 02:46:21 -0400

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""Module defines Square"""
+
+
+class Square:
+    """Square is class practice"""
+
+    def __init__(self, size=0):
+        """happens as soon the class is call/use
+        Args: size - size of the square
+
+        Raises:
+            TypeError: If size is not an int
+            ValueError: If size is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Area returns the area of the square
+        Return:
+            the area of the square
+        """
+
+        return self.__size * self.__size
