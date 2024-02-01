143dbcfc0c07e553fad61b8669b72c2fa525364d
William <titanbabrisa@gmail.com>
2024-01-31 22:17:40 -0400

    task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value), end="")
+    except Exception:
+        return False
+    else:
+        print()
+        return True
