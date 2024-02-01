1c9b1c4d431aa19c18bc2f825b8e30c0ae732f1c
William <titanbabrisa@gmail.com>
2024-01-31 22:49:44 -0400

    task 2

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     amount = 0
     for amount, i in enumerate(range(x), start=1):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
         except Exception:
             print()
             return amount - 1
