3baf6c7cb1b21566ccb02f09988f49d060a515ff
William <titanbabrisa@gmail.com>
2024-02-01 02:09:15 -0400

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""0-square contains a object
+"""This module defines a object name Square
     Class:
         Square
 """
 class Square:
     """Square is going to be empty for the moment
+        Attributes:
+            None
+        
+        Methods:
+            square():
+                Returns:
+                    None
     """
     def square():
         """square is going to be empty for the moment
