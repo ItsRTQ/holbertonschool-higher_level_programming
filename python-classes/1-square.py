eae4648d07c92e2350fac9a836e5bdc569bf943e
William <titanbabrisa@gmail.com>
2024-02-01 02:27:24 -0400

    task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Module defines Square"""
+
+
+class Square:
+    """Square is class practice"""
+
+    def __init__(self, size):
+        """happens as soon the class is call/use
+        Args: size - size of the square
+        """
+
+        self.__size = size
