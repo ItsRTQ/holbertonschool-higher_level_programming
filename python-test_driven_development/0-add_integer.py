91b6c1ca8cf12a32d7ed976c27c4a1af9c06717d
William <titanbabrisa@gmail.com>
2024-02-01 20:32:57 -0400

    task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This Module contains the definition of add_integer()"""
+
+
+def add_integer(a, b=98):
+    """add_integer adds a + b
+    Args:
+        a - First value
+        b - second value
+
+    Returns: The addition of a + b
+    """
+
+    result = 0
+    if isinstance(a, (int, float)):
+        result += int(a)
+    else:
+        raise TypeError("a must be an integer")
+    if isinstance(b, (int, float)):
+        result += int(b)
+    else:
+        raise TypeError("b must be an integer")
+    return result
