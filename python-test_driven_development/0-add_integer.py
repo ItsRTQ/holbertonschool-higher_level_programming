c12ed95c84dd33831ab6e7a5e6fdfc5b3b751d44
William <titanbabrisa@gmail.com>
2024-02-01 21:53:48 -0400

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,15 +12,13 @@ def add_integer(a, b=98):
         The addition of a + b
 
     Raises:
-        TypeError: When a or b arent int or float
+        TypeError: When a or b arent int/float
     """
 
-    if isinstance(a, (int, float)):
-        a = int(a)
-    else:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if isinstance(b, (int, float)):
-        b = int(b)
-    else:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
     return a + b
--- python-test_driven_development/example_main.py
+++ python-test_driven_development/example_main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
-a = None
-print(add_integer("2.5, 2.5"))
+
+print(add_integer(100.3, {}))
