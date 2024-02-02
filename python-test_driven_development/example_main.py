8cb22595a09a6d00d99cf8a1c8ea5f3a8bcc1735
William <titanbabrisa@gmail.com>
2024-02-02 00:57:13 -0400

    task 2

--- python-test_driven_development/example_main.py
+++ python-test_driven_development/example_main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [[-1, -2, -3], [-4, -5, -6], [-7, -8, -9]]
-print(matrix_divided(matrix, 3))
+matrix = [[-1, -2.2, 3, 0.5],[0.0, -0, -0.0, -0.5]]
+print(matrix_divided(matrix, (3,)))
