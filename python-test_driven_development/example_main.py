adb8ccbfa54379c2a123ea34c5b1bca530ea8c6c
William <titanbabrisa@gmail.com>
2024-02-04 01:38:52 -0400

    task 2

--- python-test_driven_development/example_main.py
+++ python-test_driven_development/example_main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
-var1, var2 = None, None
-say_my_name()
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix_divided()
