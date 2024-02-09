02a5c650a9c63dc27d28defb0814916ea7c936fe
William <titanbabrisa@gmail.com>
2024-02-08 22:39:14 -0400

    small changes

--- python-inheritance/testing_main.py
+++ python-inheritance/testing_main.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
-my_list = MyList()
-my_list.append(1)
-my_list.append(3)
-my_list.append(2)
-my_list.print_sorted()
-print(my_list)
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
+
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
