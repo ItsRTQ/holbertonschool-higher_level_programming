dc4ae6f205021ff4344505e9efad4f710e19ce81
William <titanbabrisa@gmail.com>
2024-02-09 01:23:38 -0400

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""This module defines function append_write"""
+
+
+def append_write(filename="", text=""):
+    """append_write, opens a file and appends into the files
+        Returns: amount of character appended
+    """
+
+    with open(filename, 'a', encoding="utf-8") as file:
+        charaters_appended = file.write(text)
+    return charaters_appended
