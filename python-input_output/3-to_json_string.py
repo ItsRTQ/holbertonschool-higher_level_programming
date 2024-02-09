7a8c1a4e30ecba5d5be7d51e7dbbabb74732e77f
William <titanbabrisa@gmail.com>
2024-02-09 01:29:38 -0400

    task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""this module defines function to_json_string"""
+
+
+import json
+def to_json_string(my_obj):
+    """this function stringifys a obj to json txt
+        Returns: the stringify data
+    """
+
+    as_json = json.dumps(my_obj)
+    return as_json
