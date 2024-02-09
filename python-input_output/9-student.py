fe8fea2a82d544cdc8ea99e9b9978a71d4926ee4
William <titanbabrisa@gmail.com>
2024-02-09 02:49:25 -0400

    task 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""This module defines class Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """This method happens as soon as a student instances is made"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """this function retrives a dictionary representation of Student obj
+            Returns: dictionary
+        """
+
+        return vars(self)
