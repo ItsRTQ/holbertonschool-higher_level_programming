299c43af9ad0f67aa2c4d90e6fabbcc90a6448fc
William <titanbabrisa@gmail.com>
2024-02-09 01:04:45 -0400

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""This module defines function read_file"""
+
+
 def read_file(filename=""):
+    """read_file, prints the content of file, using with"""
+
     with open(filename, 'r') as file:
         file_content = file.read()
         print(file_content)
