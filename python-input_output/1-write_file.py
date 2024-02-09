19d89a734b35e1d5625bc64c659e52678e8825ed
William <titanbabrisa@gmail.com>
2024-02-09 01:20:45 -0400

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,10 @@
 
 
 def write_file(filename="", text=""):
-    """write_file, opens a file and writes into the files"""
+    """write_file, opens a file and writes into the files
+        Returns: amount of character written
+    """
 
-    with open(filename, 'w') as file:
-        file.write(text)
+    with open(filename, 'w', encoding="utf-8") as file:
+        charaters_written = file.write(text)
+    return charaters_written
