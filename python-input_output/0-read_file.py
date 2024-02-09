594c223f4ff36ce670ca294e67d873915e58ec4f
William <titanbabrisa@gmail.com>
2024-02-09 01:06:16 -0400

    small changes

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,4 @@ def read_file(filename=""):
     """read_file, prints the content of file, using with"""
 
     with open(filename, 'r') as file:
-        file_content = file.read()
-        print(file_content)
+        print(file.read())
