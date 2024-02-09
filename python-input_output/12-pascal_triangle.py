f1cd98c8d39865e34b0c591aca6e329556d414ec
William <titanbabrisa@gmail.com>
2024-02-09 04:12:26 -0400

    task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""this module defines pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """this function returns a pascal list size of n
+        Returns: list of lists
+    """
+
+    pascal = []
+    if n <= 0:
+        return pascal
+    for amount, row in enumerate(range(n), start=1):
+        pascal_row = []
+        for elements in range(amount):
+            if elements == 0 or elements+1 == amount:
+                pascal_row.append(1)
+            else:
+                pascal_row.append(temp[elements] + temp[elements-1])
+        pascal.append(pascal_row.copy())
+        temp = pascal_row.copy()
+
+    return pascal
