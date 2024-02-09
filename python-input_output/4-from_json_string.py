9fad0bcbcadf2fe254b43d684bc9df6bf60842b7
William <titanbabrisa@gmail.com>
2024-02-09 01:36:21 -0400

    task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""this module defines function from_json_string"""
+import json
+
+
+def from_json_string(my_str):
+    """this function takes a json stringify transforms it obj back to an obj
+        Returns: obj
+    """
+
+    python_obj = json.loads(my_str)
+    return python_obj
