1f03a670889f296206752633b20d37ce76f7e0df
William <titanbabrisa@gmail.com>
2024-02-09 01:13:11 -0400

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""This module defines function write_file"""
+
+
+def write_file(filename="", text=""):
+    """write_file, opens a file and writes into the files"""
+
+    with open(filename, 'w') as file:
+        file.write(text)
