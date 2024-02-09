efc04776fd6b6a11be550d3e088ea4af90680452
William <titanbabrisa@gmail.com>
2024-02-09 02:01:53 -0400

    task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import json
+"""module defines function load_from_json_file"""
+
+
+def load_from_json_file(filename):
+    """this takes an obj from json file and converts it to python obj
+        Return: json -> python obj
+    """
+
+    with open(filename, 'r', encoding="utf-8") as file:
+        python_obj = json.loads(file.read())
+    return python_obj
