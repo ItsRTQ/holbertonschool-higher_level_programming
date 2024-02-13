ae355cc8456be823f26501bbedc713aaf9388f84
William <titanbabrisa@gmail.com>
2024-02-13 13:07:23 -0400

    small changes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,10 +22,10 @@ class Rectangle(Base):
                 id: id of rectangle
         """
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
