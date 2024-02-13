3ce6e34c233028c3877973ceb643251d3f43c840
William <titanbabrisa@gmail.com>
2024-02-13 19:41:36 -0400

    added square logic

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""This module defines object class Square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This class represent the geometrical shape Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """This method gets call as soon as an instance is created"""
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """This method is call when you print a square instance"""
+
+        msg = ""
+        msg += f"[Square] ({self.id})"
+        msg += f" {self.x}/{self.y}"
+        msg += f" - {self.width}"
+        return msg
