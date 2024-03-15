134cac028ab98d50615bd415b99b9ba189ebc0bd
William <titanbabrisa@gmail.com>
2024-03-15 16:05:01 -0400

    additions

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,10 @@
 -- this sql script the full description
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+SELECT column_name, data_type, character_maximum_length
+FROM information_schema.columns
+WHERE table_schema = table_name = 'first_table';
+SELECT column_name
+FROM information_schema.key_column_usage
+WHERE table_schema = table_name = 'first_table' AND constraint_name = 'PRIMARY';
+SELECT index_name, column_name
+FROM information_schema.statistics
+WHERE table_schema =table_name = 'first_table';
