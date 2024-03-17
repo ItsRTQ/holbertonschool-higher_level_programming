090c4476a5de21cd88abb65845e9f1c0be1accb0
William <titanbabrisa@gmail.com>
2024-03-17 18:16:42 -0400

    chgs

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- This script creates a user with set password
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED By 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED By 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
