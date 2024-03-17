062a894aaab8371f6ed1ba063270838fdc974a2a
William <titanbabrisa@gmail.com>
2024-03-17 17:42:14 -0400

    small changes

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- this script print the user privilage for set user of local host
-SHOW GRANT FOR 'user_0d_1'@'localhost';
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
