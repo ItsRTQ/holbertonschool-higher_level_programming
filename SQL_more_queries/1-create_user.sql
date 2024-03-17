8e5fa3a2a70014e3f5dbd9a535757d6294885392
William <titanbabrisa@gmail.com>
2024-03-17 18:13:18 -0400

    chgs

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,12 +1,4 @@
 -- This script creates a user with set password
-SELECT EXISTS (
-    SELECT 1 
-    FROM mysql.user 
-    WHERE user = 'user_0d_1' AND host = '%'
-) INTO @user_exists;
-
-IF @user_exists = 0 THEN
-    CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
-    FLUSH PRIVILEGES;
-END IF;
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED By 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
