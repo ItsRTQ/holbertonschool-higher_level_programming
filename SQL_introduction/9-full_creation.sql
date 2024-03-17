f8ae1e786f3b525768dde9da51fde2f3899bf039
William <titanbabrisa@gmail.com>
2024-03-17 15:47:46 -0400

    additions

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,10 +1,8 @@
 --this sql script creates a table with set values
-DROP TABLE IF EXISTS second_table;
-CREATE TABLE second_table(
+CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
-    score INT
-);
+    score INT);
 INSERT INTO second_table (id, name, score)
 VALUES  (1, 'John', 10),
         (2, 'Alex', 3),
