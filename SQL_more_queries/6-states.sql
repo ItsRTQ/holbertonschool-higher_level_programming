0473883ec9f3ae1a3f05e12bd55c352a7c58130f
William <titanbabrisa@gmail.com>
2024-03-18 00:47:04 -0400

    additions

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- This script creates a database and table
-CREATE database IF NOT EXISTS hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(PRIMARY KEY (id),
 id INT NOT NULL KEY AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL);
