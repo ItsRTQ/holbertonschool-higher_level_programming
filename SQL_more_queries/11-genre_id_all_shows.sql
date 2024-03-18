4821613fc4a1e66c4fc0675c00606b6852a14fab
William <titanbabrisa@gmail.com>
2024-03-18 01:47:49 -0400

    additions

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 -- This script list all shows contained in a database
-SELECT tv_shows.title, IFNULL(tv_show_genres.g_id, 'NULL') AS g_id
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.g_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
