c4079356da30f841e1b8cec72b15001b90a8a2bb
William <titanbabrisa@gmail.com>
2024-03-18 17:37:44 -0400

    additions

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,42 +1,51 @@
 #!/usr/bin/python3
-"""This module list all states from database hbtn_0e_0_usa"""
+"""This script lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import sys
 
 
+Base = declarative_base()
+
+
+class State(Base):
+    """This class is going to represent each row from the database"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+
+
 def list_states(username, password, database):
-    """This function connects to database to list data
-        ARGS:
-            username: user
-            password: passcode
-            database: targeted database
-    """
+    """Function to list all states from the database"""
+
     try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
+        mysql_conn = MySQLdb.connect(host="localhost",
+                                     port=3306,
+                                     user=username,
+                                     passwd=password,
+                                     db=database)
+        cursor = mysql_conn.cursor()
+        engine = create_engine(
+            f"mysql://{username}:{password}@localhost:3306/{database}")
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        states = session.query(State).order_by(State.id).all()
+        for state in states:
+            print(state.name)
+
+    except Exception as e:
+        print("Error:", e)
     finally:
-        if db:
-            db.close()
+        if mysql_conn:
+            mysql_conn.close()
+        if session:
+            session.close()
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        sys.exit(0)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
+        sys.exit(1)
+    username, password, database = sys.argv[1:]
     list_states(username, password, database)
