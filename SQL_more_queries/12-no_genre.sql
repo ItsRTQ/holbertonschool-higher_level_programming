9598ae99db60a1ca5542da0b8f9dbae8e760b943
William <titanbabrisa@gmail.com>
2024-03-18 01:49:55 -0400

    additions

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- This script list all shows contained in database without genre link
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title;
